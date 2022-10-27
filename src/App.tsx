1d761c570b4f035d980531542bb01749f7cd2342
Alyssa <alyssakayquigley@gmail.com>
2022-10-26 20:52:01 -0400

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 import  MovieList  from './components/MovieList'
 import SearchList from './components/SearchList';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <SearchList></SearchList>
       <MovieList></MovieList>
     </div>
