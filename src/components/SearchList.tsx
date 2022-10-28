7fbbe1fe577e9172b238ebbe170a520812ec9628
jonathangalloway <42226335+jonathangalloway@users.noreply.github.com>
2022-10-27 20:13:30 -0400

    updated search

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { Result } from '../models/PopMovie'
 import { getPopMovies } from '../services/PopularMovies'
 import { getSearchMovies } from '../services/SearchService'
+import "./MovieCard.css";
 
 
 export default function SearchList() {
   const [inputValue, setInputValue] = useState ("");
-  const [results, setSearchResults] = useState()
+  const [results, setSearchResults] = useState<Result[]>([])
   const onChange = (event: any)=>{
     event.preventDefault()
     setInputValue(event.target.value)
@@ -16,7 +17,7 @@ export default function SearchList() {
         const fetch = async () => {
       try {
         const res = await getSearchMovies(inputValue);
-        setSearchResults(res)
+        setSearchResults(res.data.results)
         console.log(res)
         // setData({ ...data, results: res.data });
       } catch (err) {
@@ -25,26 +26,28 @@ export default function SearchList() {
     };
     fetch()
    }
-  }
   
-    const searchDisplay = searchResults
-    {results.map((results)) => ()
     
-return (
+    
+   return (
     <div>
       <form className= 'searchForm'>
       <input className='searchInput' onChange={(event) => onChange(event)}/>
       <button className= 'searchButton' onClick={(event) => onSubmit(event)}>Search</button>
       </form>
-    </div>
-  )
-  }
-const useFetch = () => {
-  const [data, setData] = useState ({
-    searchItem: "",
-    results: []
-  });
-
-
-  return;
-}
+      {results.map((result) => {
+      return(
+        <div className='Movielist'>
+          {result.title}
+          <li className="MovieCard">
+            <img
+              src={`https://image.tmdb.org/t/p/w1280${result.poster_path}`}
+              alt={result.title}
+            />
+          </li>
+        </div>
+      )}
+     
+      )}
+      </div>
+   )}
